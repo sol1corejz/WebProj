e528ab67b07294350e9554de76b81f8837f8f19d
ilyap <ilyaparunov529@gmail.com>
2021-12-23 21:46:42 +0800

    Реализовано

--- src/components/Auth/Auth.jsx
+++ src/components/Auth/Auth.jsx
@@ -3,37 +3,73 @@ import s from "./Auth.module.css"
 import {useDispatch, useSelector} from "react-redux";
 import {authUserAction, newUserAction} from "../../store/reducers/authUserReducer";
 import {useNavigate} from "react-router-dom";
-
+import axios from "axios";
 
 
 const Auth = () => {
     const navigate = useNavigate();
 
-    const authState = useSelector(state => state.authUserReducer.user)
+    const authState = useSelector(state => state.authUserReducer.auth)
     const dispatch = useDispatch();
 
     const [newLog, setNewLog] = useState('')
     const [newPass, setNewPass] = useState('')
     const [newName, setNewName] = useState('')
 
-    const enter = (mail, password) => {
-        if (mail == authState.mail && password == authState.password){
-            dispatch(authUserAction({auth: true}))
-            navigate(`/main`);
+    const enter = (email, password) => {
+        const obj = {
+            email: email,
+            password: password
         }
-        else {
-            alert('Неверный логин или пароль!')
-        }
+
+        fetch('http://127.0.0.1:5000//login', {
+            method: 'post',
+            headers: {
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(obj)
+        })
+            .then( (status) => {
+                if (status.ok){
+                    dispatch(authUserAction({data: status.ok}))
+                    navigate('/main');
+                }
+                console.log(status)}
+            )
+            .catch( (error) => {
+                console.log(error)
+            });
 
     }
 
-    const reg = (mail, name, password) => {
+    const reg = (mail, password, name) => {
         let newUser = {
-            mail: mail,
+            email: mail,
             name: name,
             password: password,
         }
-        dispatch(newUserAction(newUser))
+        fetch('http://127.0.0.1:5000//register', {
+            method: 'post',
+            headers: {
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Methods": "POST, GET, PUT",
+                "Access-Control-Allow-Headers": "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With",
+                'accept': 'application/json',
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(newUser)
+        })
+            .then( (status) => {
+                console.log('Request succeeded with JSON response', status);
+                alert('Статус регистрации ' + (status.ok ? 'Успешно' : 'Не удалось'))
+            })
+            .catch(function (error) {
+                console.log('Request failed', error);
+            });
+        //dispatch(newUserAction(newUser))
         setNewPass('')
         setNewLog('')
         setNewName('')
@@ -57,8 +93,8 @@ const Auth = () => {
                 onChange={event => setNewName(event.target.value)}
             />
             <div className={s.Buttons}>
-                <button onClick={ () => enter(newLog, newPass)}>Войти</button>
-                <button onClick={ () => reg(newLog, newPass, newName)}>Зарегестрироваться</button>
+                <button onClick={() => enter(newLog, newPass)}>Войти</button>
+                <button onClick={() => reg(newLog, newPass, newName)}>Зарегестрироваться</button>
             </div>
         </div>
     );
