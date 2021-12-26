9e3fe6709110dec1e98f01e2f98fd5e7b4157dfc
Alexander Chekhov <chekhovsasha4737@gmail.com>
2021-12-26 23:17:50 +0800

    Changed database: PostgreSQL -> SQLite

--- flask/application.py
+++ flask/application.py
@@ -1,12 +1,11 @@
-from flask_sqlalchemy import SQLAlchemy
-from config import DB_CONFIG
+from config import SQLITE_FILE_NAME
 from flask import Flask
-from flask_login import LoginManager
 
 app = Flask(__name__)
-# "postgresql+psycopg2://postgres:12345@localhost:5432/messanger"
-app.config['SQLALCHEMY_DATABASE_URI'] = \
-    "postgresql+psycopg2://{user}:{password}@{host}:{port}/{database}".format(**DB_CONFIG)
+
+#app.config['SQLALCHEMY_DATABASE_URI'] = \
+#    "postgresql+psycopg2://{user}:{password}@{host}:{port}/{database}".format(**DB_CONFIG)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{SQLITE_FILE_NAME}"
+
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-
--- flask/config.py
+++ flask/config.py
@@ -1,9 +1,10 @@
 DB_CONFIG = {
     'user': 'postgres',
-    'password': '1379',
+    'password': 'su_12345',
     'host': '127.0.0.1',
     'port': '5432',
-    'database': 'messanger'
+    'database': 'messeger'
 }
+SQLITE_FILE_NAME = 'messanger.db'
 DEBUG = True
 DEFAULT_AVATAR = 'https://vraki.net/sites/default/files/inline/images/30_55.jpg'
